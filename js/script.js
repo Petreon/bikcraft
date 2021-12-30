74f9e78ad258e536981126bc935aa7852ac2fc6c
Petreon <vitorr2009@gmail.com>
2021-12-30 18:59:03 -0300

    script Parametros URL do orcamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +10,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProdutos(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProdutos);
